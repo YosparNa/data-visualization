5b2c865c15987107e6c0b0da9011a67537fe443a
YosparNa <2730094242@qq.com>
2024-11-22 11:33:15 +0800

    15.2

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,19 +1,33 @@
 import matplotlib.pyplot as plt
 
-input_values = [1, 2, 3, 4, 5]
-squares = [1, 4, 9, 16, 25]
+def main():
+    # 使用内置样式
+    plt.style.use('seaborn-v0_8')
 
-fig, ax = plt.subplots()
-ax.plot(squares, linewidth=3)
+    # 数据准备
+    x_values = [1, 2, 3, 4, 5]
+    y_values = [x**2 for x in x_values]  # 计算y值为x的平方
 
-ax.plot(input_values, squares, linewidth=3)
+    # 创建图表和一个子图
+    fig, ax = plt.subplots()
 
-# 设置图题并给坐标轴加上标签
-ax.set_title("Square Numbers", fontsize=24)
-ax.set_xlabel("Value", fontsize=14)
-ax.set_ylabel("Square of Value", fontsize=14)
+    # 绘制数据
+    ax.plot(x_values, y_values, linewidth=2)  # 绘制线图
+    ax.scatter(x_values, y_values, color='red', s=100)  # 在相同位置上绘制红色散点图
 
-# 设置刻度标记的样式
-ax.tick_params(labelsize=14)
+    # 设置图表标题和标签
+    ax.set_title("Simple Plot & Scatter", fontsize=24)
+    ax.set_xlabel("X Label", fontsize=14)
+    ax.set_ylabel("Y Label", fontsize=14)
 
-plt.show()
+    # 设置刻度标记的大小
+    ax.tick_params(axis='both', labelsize=14)
+
+    # 显示网格
+    ax.grid(True)
+
+    # 显示图表
+    plt.show()
+
+if __name__ == '__main__':
+    main()
