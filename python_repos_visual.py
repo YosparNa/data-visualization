ddc53a01780b39fb5660e9115ba1a06ac3469651
Hodaka <2841006960@qq.com>
2024-11-29 11:00:21 +0800

    增强 Python 仓库可视化
    - 添加仓库名称到链接的转换，提高交互性
    - 增加悬停文本，包含仓库所有者和描述信息
    - 添加图表标题和轴标签，提升可读性
    - 调整图表样式，使用 SteelBlue 颜色和调整透明度

--- python_repos_visual.py
+++ python_repos_visual.py
@@ -12,10 +12,23 @@ response_dict = r.json()
 print(f"Complete results: {not response_dict['incomplete_results']}")
 # 处理有关仓库的信息
 repo_dicts = response_dict['items']
-repo_names, stars = [], []
+repo_links, stars, hover_texts = [], [], []
 for repo_dict in repo_dicts:
-    repo_names.append(repo_dict['name'])
+    # 将仓库名转换为链接
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
     stars.append(repo_dict['stargazers_count'])
+    # 创建悬停文本
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    hover_text = f"{owner}<br />{description}"
+    hover_texts.append(hover_text)
 # 可视化
-fig = px.bar(x=repo_names, y=stars)
+title = "Most-Starred Python Projects on GitHub"
+labels = {'x': 'Repository', 'y': 'Stars'}
+fig = px.bar(x=repo_links, y=stars, title=title, labels=labels,
+          hover_name=hover_texts)
+fig.update_traces(marker_color='SteelBlue', marker_opacity=0.6)
 fig.show()
